b5d8314162e36b500f0ec088ac0d411ddb5cdc35
Kyryl <poronko2045@gmail.com>
2025-07-12 18:31:23 +0200

    BaseResponse model added

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Backend.SharedContracts
 📚 Shared contracts, DTOs, events, and interfaces for Pure Delivery microservices. Published as NuGet package to GitHub Packages for cross-service communication and data consistency.
 
+[1.4.0] [12.07.2025]
+-Added BaseResponse model
+
 [1.3.0] [09.07.2025]
 Removed ApiResponse model. Moved to separate project
 
---
+++ SharedContracts/SharedContracts/Domain/Models/BaseResponse.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PureDelivery.Shared.Contracts.Domain.Models
+{
+    public class BaseResponse<T>
+    {
+        public bool IsSuccess { get; set; }
+        public T? Data { get; set; }
+        public string? Message { get; set; }
+        public string? Error { get; set; }
+
+        public static BaseResponse<T> Success(T data, string? message = null)
+        {
+            return new BaseResponse<T>
+            {
+                IsSuccess = true,
+                Data = data,
+                Message = message
+            };
+        }
+
+        public static BaseResponse<T> Failure(string error)
+        {
+            return new BaseResponse<T>
+            {
+                IsSuccess = false,
+                Error = error
+            };
+        }
+    }
+}
