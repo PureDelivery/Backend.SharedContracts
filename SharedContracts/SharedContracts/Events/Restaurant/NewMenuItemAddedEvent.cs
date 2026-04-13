1845328ed1e61216f9d5669eb55279a56430b7c7
Kyryl <poronko2045@gmail.com>
2026-04-13 23:28:19 +0200

    NewMenuItemAddedEvent added for AI project

---
+++ SharedContracts/SharedContracts/Events/Restaurant/NewMenuItemAddedEvent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PureDelivery.Shared.Contracts.Events.Restaurant
+{
+    public class NewMenuItemAddedEvent
+    {
+        public Guid MenuItemId { get; set; }
+        public Guid RestaurantId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int Category { get; set; }
+        public int CaloriesPer100g { get; set; }
+        public decimal ProteinPer100g { get; set; }
+        public decimal FatPer100g { get; set; }
+        public decimal CarbsPer100g { get; set; }
+        public int WeightGrams { get; set; }
+        public int Allergens { get; set; }
+        public int DietaryTags { get; set; }
+        public bool IsPopular { get; set; }
+        public bool IsRecommended { get; set; }
+    }
+}
