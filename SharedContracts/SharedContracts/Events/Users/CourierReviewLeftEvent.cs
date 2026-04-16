a15eb3a5ac1294bb588cf73001b998367abae34c
Kyryl <poronko2045@gmail.com>
2026-04-16 16:17:58 +0200

    Events added to consume in achievment service

---
+++ SharedContracts/SharedContracts/Events/Users/CourierReviewLeftEvent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PureDelivery.Shared.Contracts.Events.Users
+{
+    public class CourierReviewLeftEvent
+    {
+        public Guid ReviewId { get; set; }
+        public Guid UserId { get; set; }
+        public Guid CourierId { get; set; }
+        public int Rating { get; set; }
+        public string Comment { get; set; } = string.Empty;
+    }
+}
---
+++ SharedContracts/SharedContracts/Events/Users/RestaurantReviewLeftEvent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PureDelivery.Shared.Contracts.Events.Users
+{
+    public class RestaurantReviewLeftEvent
+    {
+        public Guid ReviewId { get; set; }
+        public Guid UserId { get; set; }
+        public Guid RestaurantId { get; set; }
+        public int Rating { get; set; }
+        public string Comment { get; set; } = string.Empty;
+    }
+}
