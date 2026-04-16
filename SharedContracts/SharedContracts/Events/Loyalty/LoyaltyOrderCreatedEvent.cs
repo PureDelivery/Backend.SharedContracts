eaa8d92b0bc5fcdb0ca651ba82506405c04ef1eb
Kyryl <poronko2045@gmail.com>
2026-04-16 18:56:38 +0200

    New events for loyalty added

---
+++ SharedContracts/SharedContracts/Events/Loyalty/LoyaltyOrderCreatedEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PureDelivery.Shared.Contracts.Events.Loyalty
+{
+    public record LoyaltyOrderCreatedEvent
+    {
+        public Guid OrderId { get; init; }
+        public Guid UserId { get; init; }
+
+        public decimal PointsToSpend { get; init; }
+    }
+}
---
+++ SharedContracts/SharedContracts/Events/Loyalty/PaidLoyaltyEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PureDelivery.Shared.Contracts.Events.Loyalty
+{
+    public record PaidLoyaltyEvent
+    {
+        public Guid OrderId { get; init; }
+        public Guid UserId { get; init; }
+
+        public decimal PointsToAdd { get; init; }
+    }
+}
